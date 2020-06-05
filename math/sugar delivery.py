49073c0d83d05ee53d6be240261850e2adfaa3cc
Kyungyun Kim <kyknn79@naver.com>
2020-06-05 16:23:41 +0900

    hi

---
+++ math/sugar delivery.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+cnt = n
+i = 0
+
+while n - 5*i >= 0:
+    if (n - 2*i) % 3 == 0:
+        cnt = min(cnt, (n - 2*i)//3)
+    i += 1
+
+if cnt == n:
+    cnt = -1
+print(cnt)
