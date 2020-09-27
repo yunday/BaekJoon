11651a1e08a25cd202106cd51f0e1fb9eaec7837
Kyungyun Kim <kyknn79@naver.com>
2020-09-28 01:14:57 +0900

    hi

--- breadth-first search/단지번호붙이기.py
+++ breadth-first search/단지번호붙이기.py
@@ -2,38 +2,33 @@ import collections
 
 n = int(input())
 maze = [list(map(int, input())) for _ in range(n)]
-cnt = 0
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
-id_n, id_m = 0, 0
-q_count = [0]*((n * n) // 2)
+q_count = []
 
-while True:
-    state = 1
-    for _ in range(n):
-        if 1 in maze[_]:
-            id_n = _
-            id_m = maze[_].index(1)
-            state = 0
-            break
-    if state == 1:
-        break
+
+def bfs(id_n, id_m):
     queue = collections.deque([[id_n, id_m]])
-    cnt += 1
+    maze[id_n][id_m] = 0
+    p_count = 0
     while queue:
         x, y = queue.popleft()
-        for i in range(4):
-            tx = x + dx[i]
-            ty = y + dy[i]
+        for k in range(4):
+            tx = x + dx[k]
+            ty = y + dy[k]
             if tx == n or tx == -1 or ty == n or ty == -1:
                 continue
             if maze[tx][ty] == 1:
                 queue.append([tx, ty])
-                q_count[cnt-1] += 1
-                maze[tx][ty] = cnt + 1
+                p_count += 1
+                maze[tx][ty] = 0
+    return p_count + 1
 
-print(cnt)
-q_count.sort()
-for i in q_count:
-    if i != 0:
-        print(i)
+
+for i in range(n):
+    for j in range(n):
+        if maze[i][j] == 1:
+            q_count.append(bfs(i, j))
+
+print(len(q_count))
+print(*sorted(q_count))
